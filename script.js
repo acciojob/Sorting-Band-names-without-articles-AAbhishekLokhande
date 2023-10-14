564738e6b9887372bc369fa5924903572207440f
Abhishek Lokhande <115551272+AAbhishekLokhande@users.noreply.github.com>
2023-10-14 13:13:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-//your code here
+// //your code here
+// let arr = [abhi, the king, a kind man,];
+// let f = function sorting(elements, index,arrlist) {
+// 	if()
+// }
+// arr.foreach(f)
+let band = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function sortBandNames(band) {
+    const articles = ['a', 'an', 'the'];
+    const sortedBands = band.slice().sort((a, b) => {
+        const nameA = a.replace(/^(a |an |the )/i, '');
+        const nameB = b.replace(/^(a |an |the )/i, '');
+        return nameA.localeCompare(nameB);
+    });
+
+    const ul = document.createElement('ul');
+    ul.setAttribute('id', 'band');
+
+    sortedBands.forEach(band => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(band));
+        ul.appendChild(li);
+    });
+
+    document.body.appendChild(ul);
+}
+
+sortBandNames(band);
